11e9539d5f96cff4212da7186547d7ecf990650a
rafaela kasneci <rafaelakasneci1@rafaelas-MacBook-Pro.local>
2024-12-16 18:50:50 +0100

    add models

--- aplikacion/models.py
+++ aplikacion/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    item_name = models.CharField(max_length = 100, null = True, blank = True)
+    item_description = models.TextField(max_length = 1000, null = True, blank = True)
+    item_image = models.ImageField(upload_to="item/", null = True, blank = True)
